858e771b9b450e2819cf2e34d89297b36f72349a
filiphuhta <filip.huhta@arcticgroup.se>
2021-04-25 15:30:02 +0200

    formated code

--- image-gallery-server/app.js
+++ image-gallery-server/app.js
@@ -18,39 +18,40 @@ var options = {
 
 
 //GET Images (This is CORS-enabled for all origins)
-app.get("/api/images" , (request, response) => {
+app.get("/api/images", (request, response) => {
     superagent.get('https://www.flickr.com/services/rest')
-    .query(options)
-    .end((err, res) => {
+        .query(options)
+        .end((err, res) => {
 
-        if(res.body){
-        response.status(200).send(res.body);
-    }
+            if (res.body) {
+                response.status(200).send(res.body);
+            }
 
-      if (err) { return console.log(err); }
-      console.log(res.body);
-    });
+            if (err) { return console.log(err); }
+            console.log(res.body);
+        });
 });
 
 
 
 //GET Images BY tag (This is CORS-enabled for all origins)
 
-app.get("/api/images/:tag" , (request, response) => {
+app.get("/api/images/:tag", (request, response) => {
 
     options.tags = request.params.tag;
     console.log(options);
     superagent.get('https://www.flickr.com/services/rest')
-    .query(options)
-    .end((err, res) => {
+        .query(options)
+        .end((err, res) => {
 
-        if(res.body){
-        response.status(200).send(res.body);
-    }
-      if (err) { return response.status(404).send("There exist no images with that tag."); }});
-    });
+            if (res.body) {
+                response.status(200).send(res.body);
+            }
+            if (err) { return response.status(404).send("There exist no images with that tag."); }
+        });
+});
 
-    // Listen on port 3000
+// Listen on port 3000
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`imageGallery application is running on port ${port}...`));
 module.exports = app;
--- image-gallery-server/test/image.js
+++ image-gallery-server/test/image.js
@@ -9,43 +9,43 @@ chai.should();
 chai.use(chaiHttp);
 
 describe('Image API', () => {
-/**
- * Test Get images
- */
-describe('GET /api/images', () => {
-    it('should get all the images', (done) => {
-        chai.request(server)
-        .get('/api/images')
-        .end((err, response) => {
-            response.should.have.status(200);
-            response.should.be.json;
-            done();
+    /**
+     * Test Get images
+     */
+    describe('GET /api/images', () => {
+        it('should get all the images', (done) => {
+            chai.request(server)
+                .get('/api/images')
+                .end((err, response) => {
+                    response.should.have.status(200);
+                    response.should.be.json;
+                    done();
+                })
+        })
+
+        it('It should not GET all the images', (done) => {
+            chai.request(server)
+                .get('/api/image')
+                .end((err, response) => {
+                    response.should.have.status(404);
+                    done();
+                })
         })
     })
 
-    it('It should not GET all the images', (done) => {
-        chai.request(server)
-        .get('/api/image')
-        .end((err, response) => {
-            response.should.have.status(404);
-            done();
-        })
-    })
-})
-
-/**
- * Test Get images by tag 
- */
+    /**
+     * Test Get images by tag 
+     */
 
     it('should get all the images of specific tag', (done) => {
         const imageTag = "dogs";
         chai.request(server)
-        .get('/api/images/'+ imageTag)
-        .end((err, response) => {
-            response.should.have.status(200);
-            response.should.be.json;
-            done();
-        })
+            .get('/api/images/' + imageTag)
+            .end((err, response) => {
+                response.should.have.status(200);
+                response.should.be.json;
+                done();
+            })
     })
 
 
